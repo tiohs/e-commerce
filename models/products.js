7d68c80022c995e63a0c3583706786b5728c6c53
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-04-22 21:12:31 +0100

    Add

--- models/products.js
+++ models/products.js
@@ -1,14 +1,41 @@
-
-const products = [];
+const fs = require('fs');
+const path = require('path');
 
 module.exports = class Product {
     constructor (t){
         this.title = t;
     }
     save() {
-        products.push(this);
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+          );
+          fs.readFile(p, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                if(fileContent){
+                    products = JSON.parse(fileContent);
+                }
+            }
+            products.push(this);
+            fs.writeFile(p, JSON.stringify(products), err => {
+              console.log(err);
+            });
+          });
     }
-    static fetchAll(){
-        return products;
+    static fetchAll(cb){
+        const p = path.join(
+            path.dirname(process.mainModule.filename),
+            'data',
+            'products.json'
+          );
+        fs.readFile(p, (err, data)=>{
+            if(!data) {
+                return cb([]);
+            }
+            cb(JSON.parse(data));
+        });
+       
     }
 }
