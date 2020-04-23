5e546f1cd46bd266c74d02d64bafb86478dd9f94
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-04-23 13:22:59 +0100

    add new fetch

--- models/products.js
+++ models/products.js
@@ -1,41 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
+
+const getProductFromFile = cb => {
+    fs.readFile(p, (err, data)=>{
+        if(err) {
+            return cb([]);
+        }else {
+           return cb(JSON.parse(data));
+        }
+    });
+}
 module.exports = class Product {
     constructor (t){
         this.title = t;
     }
+
     save() {
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-          );
-          fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                if(fileContent){
-                    products = JSON.parse(fileContent);
-                }
-            }
+         getProductFromFile(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
               console.log(err);
             });
-          });
+         });
     }
     static fetchAll(cb){
-        const p = path.join(
-            path.dirname(process.mainModule.filename),
-            'data',
-            'products.json'
-          );
-        fs.readFile(p, (err, data)=>{
-            if(!data) {
-                return cb([]);
-            }
-            cb(JSON.parse(data));
-        });
-       
+        getProductFromFile(cb);      
     }
 }
