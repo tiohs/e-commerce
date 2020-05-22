8cb53d807d1f4173bf27237eeb3817a19f1ead9b
HamiltonMassango <hamiltonsilva551@gmail.com>
2020-05-22 19:57:03 +0600

    clear and create cart { css and module }

--- controllers/shop.js
+++ controllers/shop.js
@@ -41,22 +41,51 @@ exports.getCart = (req, res, next) => {
     .getCart()
     .then(cart => {
       return cart
-            .then(cartProducts => {
-              res.render('shop/cart', {
-                path: '/cart',
-                pageTitle: 'Your Cart',
-                products : cartProducts
-              });
-            }).catch(err => console.log(err));
-    }).catch(err => console.log(err));
+        .getProducts()
+        .then(products => {
+          res.render('shop/cart', {
+            path: '/cart',
+            pageTitle: 'Your Cart',
+            products: products
+          });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCart = (req, res, next) => {
-   const prodId = req.body.productId;
-   Product.findById(prodId, product => {
-      Cart.addProduct(prodId, product.price);
-   });
-   res.redirect('/cart');
+  const prodId = req.body.productId;
+  let fetchedCart;
+  let newQuantity = 1;
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: prodId } });
+    })
+    .then(products => {
+      let product;
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      if (product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
+      }
+      return Product.findByPk(prodId);
+    })
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.postCardDeleteProduct = (req, res, next) => {
